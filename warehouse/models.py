1a04de3e852a56db910c3d7f2132dba87706656a
Igor <iiigorrr@yandex.ru>
2024-10-10 14:11:39 +0400

    building db

---
+++ warehouse/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from typing import List
+
+
+class Base(DeclarativeBase):
+    pass
+
+class Product(Base):
+    __tablename__ = "products"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    description: Mapped[str] = mapped_column()
+    price: Mapped[int] = mapped_column()
+    stock: Mapped[int] = mapped_column()
+    
+    order_itemss:Mapped[List["OrderItem"]] = relationship(back_populates="product")
+
+class Order(Base):
+    __tablename__ = "orders"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    date: Mapped[int] = mapped_column()
+    status: Mapped[str] = mapped_column()
+
+    order_itemss:Mapped[List["OrderItem"]] = relationship(back_populates="order")
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"))
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
+    amount: Mapped[int] = mapped_column()
+
+    order: Mapped[Order] = relationship("Order", back_populates="order_itemss")
+    product: Mapped[Product] = relationship("Product", back_populates="order_itemss")
+
+
+
